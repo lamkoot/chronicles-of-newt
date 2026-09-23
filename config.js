681ea199560566d943c55bd12bb371a45f738140
lamkoot <lamkoot@gmail.com>
2026-09-23 11:04:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // CLIENT_ID komt uit Google Cloud Console (OAuth 2.0 Client ID, type "Web application").
 // Laat leeg om de app in demomodus te draaien (gegevens blijven dan alleen op dit apparaat).
 window.CHRONICLES_CONFIG = {
-  CLIENT_ID: ''
+  CLIENT_ID: '460377569040-uu8ll9u8ok4ujprmfmnj48a50pbpgt08.apps.googleusercontent.com'
 };
